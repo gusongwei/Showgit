a3337247e23c8c8a4870b91adef27ed7501bb717
顾先生 <1024330597@qq.com>
2019-02-21 19:56:02 +0800

    心怀不满删库跑路

--- Showgit/Program.cs
+++ Showgit/Program.cs
@@ -1,17 +1,1 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Showgit
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("hello，word");
-            //随便改了一下
-        }
-    }
-}
+﻿
